9bd36e52fb83013d36ae5b823c47fa136d00ee79
xyxiao <463618622@qq.com>
2016-07-26 12:04:45 +0800

    1

---
+++ src/components/Navbar.vue
@@ -0,0 +1,62 @@
+<template lang="html">
+  <header>
+    <section class="show-logo">
+      <span><i class="fa fa-bars"></i></span>
+      <a class="logo">积分商城</a>
+      <span><i class="fa fa-cart-plus" aria-hidden="true"></i></span>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+    }
+  },
+  computed: {},
+  ready: function () {},
+  attached: function () {},
+  methods: {},
+  components: {}
+}
+</script>
+
+<style lang="scss">
+  a, a:hover, a:visited, a:focus{
+    color: black;
+    text-decoration: none;
+  }
+
+  header {
+    width: 100%;
+    height: 50px;
+    font-size: 23px;
+    border-bottom: 1px solid gray;
+
+    section.show-logo {
+      padding: 8px 15px 10px 15px;
+
+      i {
+        float: left;
+        padding-top: 8px;
+        font-size: 23px;
+      }
+
+      i.fa-cart-plus {
+        float: right;
+        padding-top: 12px;
+        font-size: 23px;
+        margin-top: -40px;
+      }
+
+      .logo {
+        display: block;
+        width: 200px;
+        text-align: center;
+        margin: auto;
+      }
+    }
+  }
+
+</style>
---
+++ src/view/Dashboard.vue
@@ -0,0 +1,23 @@
+<template lang="html">
+  <Navbar></Navbar>
+</template>
+
+<script>
+import Navbar from '../components/Navbar'
+export default {
+  data: function () {
+    return {
+    }
+  },
+  computed: {},
+  ready: function () {},
+  attached: function () {},
+  methods: {},
+  components: {
+    Navbar
+  }
+}
+</script>
+
+<style lang="css">
+</style>
