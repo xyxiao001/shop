fb5630580c5f4da886b152a35f250000e00b889f
xyxiao <463618622@qq.com>
2016-07-27 17:28:15 +0800

    添加热门兑换商品

---
+++ src/components/Commodity.vue
@@ -0,0 +1,56 @@
+<template lang="html">
+  <div class="col-xs-5">
+    <a href="#" class="good-commodity">
+      <img v-bind:src="item.src" v-bind:alt="item.id" />
+      <p class="commodity-name">{{ item.name }}</p>
+      <p class="commodity-num">{{ item.num }}</p>
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+    }
+  },
+  props: ['item'],
+  computed: {},
+  ready: function () {},
+  attached: function () {},
+  methods: {},
+  components: {}
+}
+</script>
+
+<style lang="scss">
+  a.good-commodity {
+    display: inline-block;
+    margin-left: 25%;
+    width: 100%;
+    height: 180px;
+
+    img {
+      width: 100%;
+    }
+
+    p {
+      margin: 0;
+      margin-top: -20px;
+      text-align: center;
+      font-size: 14px;
+    }
+
+    p.commodity-name {
+      letter-spacing: 1px;
+      text-overflow: ellipsis;
+      white-space: nowrap;
+      overflow: hidden;
+    }
+
+    p.commodity-num {
+      margin-top: 5px;
+      color: #ab2b2b;
+    }
+  }
+</style>
--- src/view/Dashboard.vue
+++ src/view/Dashboard.vue
@@ -2,12 +2,23 @@
   <Navbar></Navbar>
   <Slide :items="slideImg"></Slide>
   <Notice :notices ="notices"></Notice>
+  <div class="hot-commodity">
+    <div class="title">
+      <a href="#">
+        热门兑换  <i class="fa fa-angle-right" aria-hidden="true"></i>
+      </a>
+    </div>
+    <div class="row">
+      <Commodity v-for="hot in hots" :item="hot"></Commodity>
+    </div>
+  </div>
 </template>
 
 <script>
 import Navbar from '../components/Navbar'
 import Slide from '../components/SlideWarp'
 import Notice from '../components/Notice'
+import Commodity from '../components/Commodity'
 export default {
   data: function () {
     return {
@@ -38,6 +49,32 @@ export default {
         '恭喜用户002成功兑换ipad Air2',
         '恭喜用户003成功兑换ipad Air2',
         '恭喜用户004成功兑换小米5'
+      ],
+      hots: [
+        {
+          id: 100,
+          src: 'http://yanxuan.nosdn.127.net/07e2057d35153f063cf042177d6a815e.png?imageView&quality=85&thumbnail=330x330',
+          name: '茶香酥200克',
+          num: '300积分'
+        },
+        {
+          id: 102,
+          src: 'http://yanxuan.nosdn.127.net/d34867f2e905f76eb7c8f46dcb2e052f.png?imageView&quality=85&thumbnail=330x330',
+          name: '带盖棉麻涤内衣收纳盒',
+          num: '500积分'
+        },
+        {
+          id: 103,
+          src: 'http://yanxuan.nosdn.127.net/4fb112a597732ea093df1d495587b04e.png?imageView&quality=85&thumbnail=330x330',
+          name: '速干防滑沐浴拖鞋',
+          num: '250积分'
+        },
+        {
+          id: 104,
+          src: 'http://yanxuan.nosdn.127.net/f99b3e5bbd6fbf86bad98cfe3ad9c2d9.png?imageView&quality=85&thumbnail=330x330',
+          name: 'Classic欧式压铸炒锅30cm',
+          num: '3400积分'
+        }
       ]
     }
   },
@@ -48,7 +85,8 @@ export default {
   components: {
     Navbar,
     Slide,
-    Notice
+    Notice,
+    Commodity
   }
 }
 </script>
@@ -56,7 +94,21 @@ export default {
 <style lang="scss">
   body {
     margin: auto;
-    max-width: 900px;
-    overflow: hidden;
+    max-width: 768px;
+    overflow-x: hidden;
+    overflow-y: scroll;
+  }
+
+  .hot-commodity {
+    width: 100%;
+
+    .title {
+      height: 60px;
+      text-align: center;
+      font-size: 20px;
+      padding-top: 15px;
+      line-height: 1.5;
+      color: #333;
+    }
   }
 </style>
